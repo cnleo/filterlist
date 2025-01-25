5a2448b3f097a0a161a309aadbe5b4b996875053
cnleo <github@cosmicnova.de>
2025-01-25 22:58:04 +0100

    Create filter_rules.py

---
+++ filter_rules.py
@@ -0,0 +1,50 @@
+# filter_regeln.py
+
+# Diese Liste enthält alle Filterregeln. Jede Regel ist ein Array mit spezifischen Informationen.
+filter_regeln = [
+    # wordlists, include-domains, exclude-domains, upward(N)
+   # [["Beispieltext1"], [], ["domain1.com", "domain2.com", [".cssattribute"]], 2],  # Exkludierte Domains und CSS-Attribute
+   # [["Beispieltext2"], ["domain1.com", "domain2.com", [".cssattribute", ".cssattribute2"]], [], 1],  # Inkludierte Domains und CSS-Attribute
+   # [["Noch ein Text", "Ein anderer Text"], [], [], 3],  # Global gültig, 3 divs nach oben
+   # [["Testtext"], ["includedomain.com"], ["excludedomain.com", [".cssclass"]], ],  # Test leeres updward element; default 1
+   # [["Testtext"], ["includedomain.com"], ["excludedomain.com", [".cssclass"]] ]  # Test leeres updward array; default 1
+
+    # Person
+    [["trump"],[],[],],
+    [["elon musk"],[],[],],
+    [["putin"],[],[],],
+
+    # Boycott
+    [["AZDelivery"],[],[],],
+    [["OTTO"],[],[],],
+
+    # infatillity
+    [["Worse"],[],[],],
+    [["fuck"],[],[],],
+    [["idioten"],[],[],],
+    [["verheerend"],[],[],],
+    [["Hecklers"],[],[],],
+    [["wtf"],[],[],],
+
+    # Media
+    [["ard"],[],[],],
+    [["zdf"],[],[],],
+    [["bild"],[],[],],
+    [["twitter"],[],[],],
+    [["tagesschau"],[],[],],
+    [["Fokus"],[],[],],
+    [["Spiegel"],[],[],],
+    [["computerbild"],[],[],],
+    
+    # Politics
+    [["cdu"],[],[],],
+    [["csu"],[],[],],
+    [["die linke"],[],[],],
+    [["die grünen"],[],[],],
+    [["afd"],[],[],],
+    [["fdp"],[],[],],
+    [["spd"],[],[],],
+    [["bundeswehr"],[],[],],
+    [["panzer"],[],[],],
+
+]
