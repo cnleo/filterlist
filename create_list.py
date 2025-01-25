2b06ca34cd59d6fd0fc3df1b7dc01e8070e535e7
cnleo <github@cosmicnova.de>
2025-01-25 22:14:34 +0100

    Update create_list.py feat new rulesets

--- create_list.py
+++ create_list.py
@@ -1,10 +1,12 @@
+#https://github.com/gorhill/uBlock/wiki/Static-filter-syntax
+
 # Filterregeln: Jedes Element ist ein Array mit [Wörter, inkludierte Domains, exkludierte Domains, upward-divs]
 filter_regeln = [
-    [["Beispieltext1"], ["example.com", "another-examplea.com"], [], 2],  # Inkludierte Domains, 2 divs nach oben
-    [["Beispieltext2"], [], ["testseite.de", "noch-eine-seite.de"], 1],  # Exkludierte Domains, 1 div nach oben
+    [["Beispieltext1"], [], ["domain1.com", "domain2.com", [".cssattribute"]], 2],  # Exkludierte Domains und CSS-Attribute
+    [["Beispieltext2"], ["domain1.com", "domain2.com", [".cssattribute", ".cssattribute2"]], [], 1],  # Inkludierte Domains und CSS-Attribute
     [["Noch ein Text", "Ein anderer Text"], [], [], 3],  # Global gültig, 3 divs nach oben
-    [["Noch ein Text", "Ein anderer Text"], [], []],  # Kein upward-divs angegeben, Standardwert 1
-    [["Noch ein Text", "Ein anderer Text"], [], []],  # Noch ein Beispiel ohne upward-divs
+    [["Testtext"], ["includedomain.com"], ["excludedomain.com", [".cssclass"]], ],  # Test leeres updward element; default 1
+    [["Testtext"], ["includedomain.com"], ["excludedomain.com", [".cssclass"]] ]  # Test leeres updward array; default 1
 ]
 
 # Ziel-Tags (z. B. 'a', 'p', 'div', etc.)
@@ -12,30 +14,70 @@ tags = ["a", "p"]
 
 # Generiere Filter
 filter_liste = []
+global_filter_liste = []
+
 for regel in filter_regeln:
     woerter = regel[0]  # Wörter, die gefiltert werden sollen
     include_domains = regel[1]  # Inkludierte Domains
     exclude_domains = regel[2]  # Exkludierte Domains
-    upward_divs = regel[3] if len(regel) > 3 and regel[3] is not None else 1  # Standardwert für upward-divs ist 1, falls nicht angegeben oder leer
+    upward_divs = regel[3] if len(regel) > 3 and regel[3] is not None else 1  # Standardwert für upward-divs ist 1, falls nicht angegeben
 
     for wort in woerter:
         # Die Anpassung für :upward(div, N) mit nth-of-type()
-        tag_filters = ",".join([f"{tag}:has-text(/\\b{wort}\\b/i):upward(div:nth-of-type({upward_divs}))" for tag in tags])
-        
-        # Standard: Gilt für alle Seiten (zuerst prüfen, um Gewichtung der Regeln zu beachten)
-        #if not include_domains and not exclude_domains:
-        if not include_domains:
-            filter_liste.append(f"##{tag_filters}")
-        
+        # Wenn upward_divs > 1, wird nth-of-type(N) genutzt
+        upward_filter = f":upward(div:nth-of-type({upward_divs}))" if upward_divs > 1 else ":upward(div)"
+
+        tag_filters = ",".join([f"{tag}:has-text(/\\b{wort}\\b/i){upward_filter}" for tag in tags])
+
+        # Funktion für das Anwenden von CSS-Attributen auf Domains
+        def apply_css_attributes(domains, tag_filters, is_include):
+            filter_strings = []
+            domain_string = ",".join([domain for domain in domains if isinstance(domain, str)])  # Domains in Komma-Notation
+            css_attributes = [domain for domain in domains if isinstance(domain, list)]  # CSS-Attribute extrahieren
+            
+            if domain_string:  # Falls es Domains gibt
+                if is_include:
+                    filter_strings.append(f"{domain_string}##{tag_filters}")  # Für include-Domains
+                else:
+                    filter_strings.append(f"{domain_string}#@#{tag_filters}")  # Für exclude-Domains
+
+            # CSS-Attribute an Domain hinzufügen
+            if css_attributes:
+                for css_attr in css_attributes[0]:
+                    if is_include:
+                        filter_strings.append(f"{domain_string}##{css_attr}:has-text(/\\b{wort}\\b/i){upward_filter}")
+                    else:
+                        filter_strings.append(f"{domain_string}#@#{css_attr}:has-text(/\\b{wort}\\b/i){upward_filter}")
+            
+            return filter_strings
+
+        # **Globale Filter sollen immer vor den anderen kommen, auch wenn keine include oder exclude Domains vorhanden sind**
+        if not include_domains and not exclude_domains:
+            global_filter_liste.append(f"##{tag_filters}")
+
         # Falls exkludierte Domains definiert sind
         if exclude_domains:
-            exclude_filter = f"{','.join(exclude_domains)}#@#{tag_filters}"
-            filter_liste.append(exclude_filter)
-        
+            # Globalen Filter immer vor den exkludierten Domains
+            filter_liste.append(f"##{tag_filters}")  # Füge globalen Filter hinzu
+
+            filter_strings = apply_css_attributes(exclude_domains, tag_filters, is_include=False)
+            filter_liste.extend(filter_strings)
+
         # Falls inkludierte Domains definiert sind
         elif include_domains:
-            for domain in include_domains:
-                filter_liste.append(f"{domain}##{tag_filters}")
+            filter_strings = apply_css_attributes(include_domains, tag_filters, is_include=True)
+            filter_liste.extend(filter_strings)
+
+        # Falls keine CSS-Attribute gesetzt sind, werden die Tags für a, p hinzugefügt
+        if not any(isinstance(domain, list) for domain in include_domains + exclude_domains):
+            # Wenn keine CSS-Attribute gesetzt wurden, füge Tags wie a, p hinzu
+            if include_domains:
+                filter_liste.append(f"{','.join(include_domains)}##{','.join(tags)}:has-text(/\\b{wort}\\b/i){upward_filter}")
+            if exclude_domains:
+                filter_liste.append(f"{','.join(exclude_domains)}#@#{','.join(tags)}:has-text(/\\b{wort}\\b/i){upward_filter}")
+
+# Füge alle globalen Filter an den Anfang der Liste
+filter_liste = global_filter_liste + filter_liste
 
 # Speichere die Filter in einer Datei
 with open("ublock_filter.txt", "w", encoding="utf-8") as file:
